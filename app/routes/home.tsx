2b3e404f07418ff4d3269c8b98cfbd23f15c31fd
IsmailHossenEn <ismailhossen.en@gmail.com>
2025-11-23 03:39:46 +0600

    Navbar done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,28 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResAnalyzer" },
+    { name: "description", content: "Analyzed Feedback from smart source" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
-}
+    export default function Home() {
+      return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+          <Navbar/>
+          <section className="main-section">
+              <div className="page-heading">
+                    <h1>Track Your applications & Resume Ratings</h1>
+                  <h2>Review your submission and check AI powered feedback</h2>
+              </div>
+          </section>
+
+          {resumes.map((resume:any)=>(
+              <div>
+                  <h1>{resume.jobTitle}</h1>
+              </div>
+          ))}
+      </main>
+    }
